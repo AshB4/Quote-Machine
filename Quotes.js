e7ab55087193d18eef2f48fbfe0bf51026e29754
Ashley B <109474630+AshB4@users.noreply.github.com>
2023-01-18 15:23:34 -0600

    made changes
    for better or worse

--- Quotes.js
+++ Quotes.js
@@ -7,47 +7,31 @@ function App() {
         const fetchData = async () =>
             async function fetchData()
 
-        const response = await fetch('https://type.fit/api/quoteshttps://api.quotable.io/random');
+                const response = await fetch('https://type.fit/api/quoteshttps://api.quotable.io/random');
         const data = await response.json();
         setQuotes(data);
-    })
+        setRandomQuote(data);
+    },[]);
 
 let RandomIndex = Math.floor(Math.random()*data,length);
-        setRandomQuote(data,[RandomIndex])
-const changeColors = { "#0c0e87" ,"#53acef" ,"#3a4b9e" ,"#201099" ,"#682da3" ,"#cb8cff" ,"#6407f9"}
+setRandomQuote(data,[RandomIndex])
+const changeColors = { "#0c0e87" ;"#53acef" ;"#3a4b9e" ;"#201099" ;"#682da3" ;"#cb8cff" ;"#6407f9"}
 const getNewQuote = () =>{
     let getNewQuote = Math.floor(Math.random()*data,length);
     setRandomQuote(randomQuotes,[RandomIndex]);
     setColors = math.floor(Math.random()*colors,length);
 }
-
 return{
-    <div {{styles="background: colors" min-height: 100vh}}>
-        <div className="container"> Quote of the Day! </div>
-            <><div className="jumbotron"></div><><div className="card"></div>
-                <div className="card-body">
-                    {randomQuotes ? {}
-                        :
-                    }
+    <div {{styles=background: colors min-height: 100}}>
+    <><div className="container"> Quote of the Day! </div><><div className="jumbotron"></div><><div className="card"></div>
+            <div className="card-body">
+                {randomQuotes ? {}
+                    :
 
-                </><h5 className="card-title">{randomQuotes, author || "No Author"}</h5>
-                <p className="card-text">&quote;{randomQuotes,text}&quote;</p></>
-            </div>
-            <h2>Loading...</h2>
-        }
-    }}
-        <div className="row"><button onClick= {getNewQuote} newClass="btn btn-primary ml-3">New Quote</button>
-        <a class="twitter-share-button"
-  href="https://twitter.com/intent/tweet?text=Hello%20world">
-{"randomQuote.text" + '"-"'+"randomQuote.author" }
-} target"_blank" className="btn btn-light ml-3">
-<i className=" fa fa-twitter"></i>
- </a>
-</div>
-</div>
-</div> 
-        </div>
-    ;
-}}
-ReactDOM.render<App/>document.getElementById('app')
-</div>
+
+                }
+
+
+                export default App;
+                <div className="container">
+                </div> </></></></>}
